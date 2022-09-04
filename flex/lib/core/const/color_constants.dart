0ed883eb2c512ee2b1f3362e4bc0c6f972013876
Bina Mobisa <mobisakwamboka@gmail.com>
2022-09-04 12:36:27 +0300

    constant files

---
+++ flex/lib/core/const/color_constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color homeBackground = const Color(0xFFFFFFFF);
+  static Color homeBrowseTextColor = const Color.fromARGB(255, 14, 14, 14);
+  static Color homeFullBodyWorkoutColor = const Color.fromARGB(110, 255, 255, 255);
+  static Color homeCategoriesColor = const Color.fromARGB(110, 255, 255, 255);
+  static Color homeTopWorkoutText = const Color.fromARGB(110, 255, 255, 255);
+  static Color homeTextColor = const Color.fromARGB(255, 18, 2, 2);
+  static Color homefirstContainerColor = const Color.fromARGB(255, 102, 175, 239);
+  static Color homesecondContainerColor = const Color.fromARGB(255, 237, 105, 200);
+  static Color homethirdContainerColor = const Color.fromARGB(255, 54, 19, 100);
+  static Color homeYogaTextColor = const Color(0xFFFFFFFF);
+  static Color homeStrengthTrainingColor = const Color(0xFFFFFFFF);
+  static Color analyticsPageColor = const Color.fromARGB(255, 6, 6, 6);
+  static Color analyticsTodayColor = const Color.fromARGB(255, 234, 19, 177);
+  static Color analyticsActiveCaloriesTextColor =  const Color.fromARGB(111, 226, 225, 225);
+  static Color analyticsTabBarIconActiveColor = const Color.fromARGB(255, 232, 19, 153);
+  static Color analyticsTabBarIconInactiveColor = const Color.fromARGB(255, 9, 9, 9);
+}
